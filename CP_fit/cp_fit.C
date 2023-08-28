82894a2471160858f6cb87f337ff8427297003d1
Szymon Gamrat <szymon.gamrat@student.uj.edu.pl>
2023-08-28 16:38:15 +0200

    16:37

--- CP_fit/cp_fit.C
+++ CP_fit/cp_fit.C
@@ -1,11 +1,12 @@
 #include "../../Include/Codes/interference.h"
 #include "../../Include/Codes/kloe_class.h"
 #include "../../Include/const.h"
+#include "chain_init.C"
+
 #include <TROOT.h>
 #include <TTree.h>
 #include <TFile.h>
 #include <TChain.h>
-#include <THStack.h>
 #include <Math/Minimizer.h>
 #include <Math/Factory.h>
 #include <Math/Functor.h>
@@ -13,28 +14,6 @@
 #include <TMath.h>
 
 using namespace KLOE;
-
-inline void chain_init(TChain *chain_init)
-{
-    TString fullname = "",
-    dirnamemc = "MONTE_CARLO", dirnamedata = "DATA",
-    filenamemc = "mc_stream62_mccard2_", filenamedata = "data_stream42_",
-    extension = ".root";
-
-    for(Int_t i = 1; i <= 56; i++)
-    {
-    	fullname = "/internal/big_one/4/users/gamrat/old_root_files/" + dirnamedata + "/" + filenamedata + i + extension;
-    	chain_init->Add(fullname);
-    }
-    for(Int_t i = 1; i <= 56; i++)
-    {
-        if( i != 7)
-        {
-            fullname = "/internal/big_one/4/users/gamrat/old_root_files/" + dirnamemc + "/" + filenamemc + i + extension;
-    	    chain_init->Add(fullname);
-        }
-    }
-};
 
 void cp_fit(TString mode = "")
 {
