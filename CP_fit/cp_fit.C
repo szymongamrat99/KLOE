38accb44b304ca5152abb1608ad6e54b8387fd87
Szymon Gamrat <szymon.gamrat@student.uj.edu.pl>
2023-08-30 16:32:19 +0200

    Working split and mcbcg

--- CP_fit/cp_fit.C
+++ CP_fit/cp_fit.C
@@ -12,6 +12,7 @@
 #include <Math/Functor.h>
 #include <TCanvas.h>
 #include <TMath.h>
+#include <TRatioPlot.h>
 
 using namespace KLOE;
 
@@ -29,89 +30,77 @@ void cp_fit(TString mode = "")
     chain_init(chain);
 
     UChar_t mcflag;
-    Float_t Dtboostlor, Dtmc;
+    Float_t Dtboostlor, Dtmc, Chi2, minv4gam, Kchrec[9], Qmiss;
 
     chain->SetBranchAddress("mcflag", &mcflag);
 
     chain->SetBranchAddress("Dtboostlor", &Dtboostlor);
     chain->SetBranchAddress("Dtmc", &Dtmc);
 
+    chain->SetBranchAddress("Dtmc", &Dtmc);
+
+    chain->SetBranchAddress("Chi2", &Chi2);
+    chain->SetBranchAddress("minv4gam", &minv4gam);
+    chain->SetBranchAddress("Kchrec", Kchrec);
+    chain->SetBranchAddress("Qmiss", &Qmiss);
+
     chain->AddFriend("h = h1", "../../../old_root_files/mctruth.root");
 
     UInt_t nentries = chain->GetEntries();
-    UInt_t *entries = new UInt_t[chann_num];
 
-    entries[0] = chain->GetEntries("h.mctruth == 1");
-    entries[1] = chain->GetEntries("h.mctruth == 3");
-    entries[2] = chain->GetEntries("h.mctruth == 4");
-    entries[3] = chain->GetEntries("h.mctruth == 5");
-    entries[4] = chain->GetEntries("h.mctruth == 6");
-    entries[5] = chain->GetEntries("h.mctruth == 7");
-    entries[6] = chain->GetEntries("h1.mcflag == 0");
-
-    UInt_t nbins = 181;
     Double_t x_min = -90.0, x_max = 90.0;
+    UInt_t nbins = (x_max - x_min) + 1;
 
-    interference event(mode, entries, nbins, x_min, x_max);
+    Double_t split[3] = {-30.0, 0.0, 30.0};
 
-    for(UInt_t i = 0; i < chann_num; i++) entries[i] = 0;
-
+    interference event(mode, nbins, x_min, x_max, split);
+    
+    
     for(UInt_t i = 0; i < nentries; i++)
     {
         chain->GetEntry(i);
         tree->GetEntry(i);
 
-        if(mcflag == 1)
+        if(Chi2 < 40 && abs(minv4gam - m_k0) < 76 && abs(Kchrec[5] - m_k0) < 1.2 && Qmiss < 3.75)
         {
-            if(mctruth == 1)
+            if(mcflag == 1)
             {
-                event.time_diff_gen[entries[0]] = Dtmc;
-                event.time_diff[0][entries[0]] = Dtboostlor;
+                if(mctruth == 1)
+                {
+                    event.time_diff_gen.push_back(Dtmc);
+                    event.time_diff[0].push_back(Dtboostlor);
+                }
 
-                entries[0]++;
+                if(mctruth == 3)
+                {
+                    event.time_diff[1].push_back( Dtboostlor);
+                }
+
+                if(mctruth == 4)
+                {
+                    event.time_diff[2].push_back(Dtboostlor);
+                }
+
+                if(mctruth == 5)
+                {
+                    event.time_diff[3].push_back(Dtboostlor);
+                }
+
+                if(mctruth == 6)
+                {
+                    event.time_diff[4].push_back(Dtboostlor);
+                }
+
+                if(mctruth == 7)
+                {
+                    event.time_diff[5].push_back(Dtboostlor);
+                }
             }
 
-            if(mctruth == 3)
+            if(mcflag == 0)
             {
-                event.time_diff[1][entries[1]] = Dtboostlor;
-
-                entries[1]++;
+                event.time_diff_data.push_back(Dtboostlor);
             }
-
-            if(mctruth == 4)
-            {
-                event.time_diff[2][entries[2]] = Dtboostlor;
-
-                entries[2]++;
-            }
-
-            if(mctruth == 5)
-            {
-                event.time_diff[3][entries[3]] = Dtboostlor;
-
-                entries[3]++;
-            }
-
-            if(mctruth == 6)
-            {
-                event.time_diff[4][entries[4]] = Dtboostlor;
-
-                entries[4]++;
-            }
-
-            if(mctruth == 7)
-            {
-                event.time_diff[5][entries[5]] = Dtboostlor;
-
-                entries[5]++;
-            }
-        }
-
-        if(mcflag == 0)
-        {
-            event.time_diff[6][entries[6]] = Dtboostlor;
-
-            entries[6]++;
         }
 
     }
@@ -134,15 +123,28 @@ void cp_fit(TString mode = "")
     const Double_t init_vars[num_of_vars] = {Re, M_PI*Im_nonCPT/180., 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0}, 
                    step[num_of_vars] = {1E-5, 1E-5, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05};
 
-    Double_t limit_span = 1.0;
+    Double_t limit_span_signal = 0.3, limit_span = 0.3 , limit_pars = 0.3;
 
-    minimum->SetLimitedVariable(0, "Real part",      init_vars[0], step[0], init_vars[0] - limit_span*init_vars[0], init_vars[0] + limit_span*init_vars[0]);
-    minimum->SetLimitedVariable(1, "Imaginary part", init_vars[1], step[1], init_vars[1] - limit_span*init_vars[1], init_vars[1] + limit_span*init_vars[1]);
-    minimum->SetLimitedVariable(2, "Norm signal",    init_vars[2], step[2], init_vars[2] - limit_span*init_vars[2], init_vars[2] + limit_span*init_vars[2]);
-    minimum->SetLimitedVariable(3, "Norm left DC wall",     init_vars[3], step[3], init_vars[3] - limit_span*init_vars[3], init_vars[3] + limit_span*init_vars[3]);
-    minimum->SetLimitedVariable(4, "Norm left beam pipe",     init_vars[4], step[4], init_vars[4] - limit_span*init_vars[4], init_vars[4] + limit_span*init_vars[4]);
-    minimum->SetLimitedVariable(5, "Norm right beam pipe",     init_vars[5], step[5], init_vars[5] - limit_span*init_vars[5], init_vars[5] + limit_span*init_vars[5]);
-    minimum->SetLimitedVariable(6, "Norm right DC wall",     init_vars[6], step[6], init_vars[6] - limit_span*init_vars[6], init_vars[6] + limit_span*init_vars[6]);
+    minimum->SetLimitedVariable(0, "Real part",      init_vars[0], step[0], init_vars[0] - limit_pars*init_vars[0], init_vars[0] + limit_pars*init_vars[0]);
+    minimum->SetLimitedVariable(1, "Imaginary part", init_vars[1], step[1], init_vars[1] - limit_pars*init_vars[1], init_vars[1] + limit_pars*init_vars[1]);
+    minimum->SetLimitedVariable(2, "Norm signal",    init_vars[2], step[2], init_vars[2] - limit_span_signal*init_vars[2], init_vars[2] + limit_span_signal*init_vars[2]);
+
+    if(x_min > -30.0 && x_max < 30.0)
+    {
+        minimum->SetFixedVariable(3, "Norm left DC wall",     init_vars[3]);
+        minimum->SetLowerLimitedVariable(4, "Norm left beam pipe",     init_vars[4], step[4], 0.0);
+        minimum->SetLowerLimitedVariable(5, "Norm right beam pipe",     init_vars[5], step[5], 0.0);
+        minimum->SetFixedVariable(6, "Norm right DC wall",     init_vars[6]);
+    }
+    else
+    {
+        minimum->SetLowerLimitedVariable(3, "Norm left DC wall",     init_vars[3], step[3], 0.0);
+        minimum->SetLowerLimitedVariable(4, "Norm left beam pipe",     init_vars[4], step[4], 0.0);
+        minimum->SetLowerLimitedVariable(5, "Norm right beam pipe",     init_vars[5], step[5], 0.0);
+        minimum->SetLowerLimitedVariable(6, "Norm right DC wall",     init_vars[6], step[6], 0.0);
+    }
+
+
     minimum->SetLimitedVariable(7, "Norm omega",     init_vars[7], step[7], init_vars[7] - limit_span*init_vars[7], init_vars[7] + limit_span*init_vars[7]);
     minimum->SetLimitedVariable(8, "Norm three",     init_vars[8], step[8], init_vars[8] - limit_span*init_vars[8], init_vars[8] + limit_span*init_vars[8]);
     minimum->SetLimitedVariable(9, "Norm semi",      init_vars[9], step[9], init_vars[9] - limit_span*init_vars[9], init_vars[9] + limit_span*init_vars[9]);
@@ -162,76 +164,79 @@ void cp_fit(TString mode = "")
     std::cout << "Norm semi" << " " << minimum->X()[9] << std::endl;
     std::cout << "Norm other bcg" << " " << minimum->X()[10] << std::endl;
 
-    TH1 *histo[chann_num - 1];
-
     Double_t par[2] = {minimum->X()[0], minimum->X()[1]};
 
-    for(UInt_t i = 0; i < chann_num - 1; i++)
+    for(UInt_t i = 0; i < chann_num; i++)
     {
-        histo[i] = new TH1F(("histogram" + std::to_string(i)).c_str(), "", nbins, x_min, x_max);
-    }
-
-    for(UInt_t i = 0; i < chann_num - 2; i++)
-    {
-        for(UInt_t j = 0; j < entries[i]; j++)
+        for(UInt_t j = 0; j < event.time_diff[i].size(); j++)
         {
             if(i == 0){
-                histo[i]->Fill(event.time_diff[i][j], event.interf_function(event.time_diff_gen[j]));
+                event.frac[i]->Fill(event.time_diff[i][j], event.interf_function(event.time_diff_gen[j], 0, par));
             }
             else if(i == 1)
             {  
-                if(event.time_diff[i][j] < -30.0)
-                    histo[i]->Fill(event.time_diff[i][j], minimum->X()[3]);
-                else if(event.time_diff[i][j] > -30.0 && event.time_diff[i][j] < 0.0)
-                    histo[i]->Fill(event.time_diff[i][j], minimum->X()[4]);
-                else if(event.time_diff[i][j] > 0.0 && event.time_diff[i][j] < 30.0)
-                    histo[i]->Fill(event.time_diff[i][j], minimum->X()[5]);
-                else if(event.time_diff[i][j] > 30.0)
-                    histo[i]->Fill(event.time_diff[i][j], minimum->X()[6]);
+                if(event.time_diff[i][j] < event.left_x_split)
+                    event.frac[i]->Fill(event.time_diff[i][j], minimum->X()[3]);
+                else if(event.time_diff[i][j] > event.left_x_split && event.time_diff[i][j] < event.center_x_split)
+                    event.frac[i]->Fill(event.time_diff[i][j], minimum->X()[4]);
+                else if(event.time_diff[i][j] > event.center_x_split && event.time_diff[i][j] < event.right_x_split)
+                    event.frac[i]->Fill(event.time_diff[i][j], minimum->X()[5]);
+                else if(event.time_diff[i][j] > event.right_x_split)
+                    event.frac[i]->Fill(event.time_diff[i][j], minimum->X()[6]);
             }
             else
             {
-                histo[i]->Fill(event.time_diff[i][j]);
+                event.frac[i]->Fill(event.time_diff[i][j]);
             }
         }
     }
 
-    histo[0]->Scale(minimum->X()[2]*histo[0]->GetEntries() / histo[0]->Integral(0, nbins + 1) );
+    for(UInt_t j = 0; j < event.time_diff_data.size(); j++)
+    {
+        event.data->Fill(event.time_diff_data[j]);
+    }
 
-    histo[2]->Scale(minimum->X()[7]*histo[2]->GetEntries() / histo[2]->Integral(0, nbins + 1) );
-    histo[3]->Scale(minimum->X()[8]*histo[3]->GetEntries() / histo[3]->Integral(0, nbins + 1) );
-    histo[4]->Scale(minimum->X()[9]*histo[4]->GetEntries() / histo[4]->Integral(0, nbins + 1) );
-    histo[5]->Scale(minimum->X()[10]*histo[5]->GetEntries() / histo[5]->Integral(0, nbins + 1) );
+    event.frac[0]->Scale(minimum->X()[2]*event.frac[0]->GetEntries() / event.frac[0]->Integral(0, nbins + 1) );
 
-    histo[chann_num - 2]->Add(histo[0]);
-    histo[chann_num - 2]->Add(histo[1]);
-    histo[chann_num - 2]->Add(histo[2]);
-    histo[chann_num - 2]->Add(histo[3]);
-    histo[chann_num - 2]->Add(histo[4]);
-    histo[chann_num - 2]->Add(histo[5]);
+    event.frac[2]->Scale(minimum->X()[7]*event.frac[2]->GetEntries() / event.frac[2]->Integral(0, nbins + 1) );
+    event.frac[3]->Scale(minimum->X()[8]*event.frac[3]->GetEntries() / event.frac[3]->Integral(0, nbins + 1) );
+    event.frac[4]->Scale(minimum->X()[9]*event.frac[4]->GetEntries() / event.frac[4]->Integral(0, nbins + 1) );
+    event.frac[5]->Scale(minimum->X()[10]*event.frac[5]->GetEntries() / event.frac[5]->Integral(0, nbins + 1) );
 
-    histo[0]->SetLineColor(kRed);
-    histo[1]->SetLineColor(kGreen);
-    histo[2]->SetLineColor(kViolet);
-    histo[3]->SetLineColor(kCyan);
-    histo[4]->SetLineColor(kBlue);
-    histo[5]->SetLineColor(kGreen - 1);
-    histo[6]->SetLineColor(kBlack);
-    histo[7]->SetLineColor(kGray - 1);
+    for(UInt_t i = 0; i < chann_num; i++)
+    {
+        event.mc_sum->Add(event.frac[i]);
+        event.frac[i]->SetLineColor(chann_color[i]);
+    }
+
+    event.mc_sum->SetLineColor(mcsum_color);
+    event.data->SetLineColor(data_color);
 
     TCanvas *c1 = new TCanvas("c1", "", 790, 790);
 
-    histo[7]->Draw("HIST");
-    histo[6]->Draw("PE1SAME");
-    histo[0]->Draw("HISTSAME");
-    histo[1]->Draw("HISTSAME");
-    histo[2]->Draw("HISTSAME");
-    histo[3]->Draw("HISTSAME");
-    histo[4]->Draw("HISTSAME");
-    histo[5]->Draw("HISTSAME");
+    TRatioPlot *rp = new TRatioPlot(event.mc_sum, event.data, "diffsig");
 
+    rp->Draw();
 
-    c1->Print("dupa.png");
+    //event.mc_sum->Draw("HIST");
+    //event.data->Draw("PE1SAME");
 
+    rp->GetLowerRefGraph()->SetMinimum(-5);
+    rp->GetLowerRefGraph()->SetMaximum(5);
+
+    rp->GetLowerRefYaxis()->SetLabelSize(0.02);
+
+    rp->GetUpperRefYaxis()->SetRangeUser(0.0,2500.0);
+
+    rp->GetUpperPad()->cd();
+
+    for(UInt_t i = 0; i < chann_num; i++)
+    {
+        event.frac[i]->Draw("HISTSAME");
+    }
+
+    c1->Print("split_fit_with_corr.png");
+
+    delete rp;
 
 }
