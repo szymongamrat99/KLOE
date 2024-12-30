227d397e8e9c3cc0bdb60ff313b025e9d90c98c9
Szymon Gamrat <szymon.gamrat@student.uj.edu.pl>
2024-12-30 01:04:28 +0100

    2024-12-30_0104

--- Subanalysis/GeneratedVars/src/split_channels.cpp
+++ Subanalysis/GeneratedVars/src/split_channels.cpp
@@ -13,16 +13,19 @@ Int_t split_channels(Int_t firstFile, Int_t lastFile)
 
     BaseKinematics baseKin;
 
-    chain = new TChain("h1");
+    chain = new TChain("INTERF/h1");
     chain_init(chain, firstFile, lastFile);
 
-    chain->SetBranchAddress("nTMC", &interfcommon_.ntmc);
-    chain->SetBranchAddress("nVtxMC", &interfcommon_.nvtxmc);
+    UChar_t pidmc[50], vtxmc[50], mother[50], mctruth, mcflag;
 
-    chain->SetBranchAddress("PidMC", interfcommon_.pidmc);
-    chain->SetBranchAddress("VtxMC", interfcommon_.vtxmc);
-    chain->SetBranchAddress("Mother", interfcommon_.mother);
-    // chain->SetBranchAddress("mctruth", &baseKin.mctruth);
+    chain->SetBranchAddress("ntmc", &interfcommon_.ntmc);
+    chain->SetBranchAddress("nvtxmc", &interfcommon_.nvtxmc);
+
+    chain->SetBranchAddress("pidmc", pidmc);
+    chain->SetBranchAddress("vtxmc", vtxmc);
+    chain->SetBranchAddress("mother", mother);
+    chain->SetBranchAddress("mctruth", &mctruth);
+    chain->SetBranchAddress("mcflag", &mcflag);
 
     Int_t nentries = (Int_t)chain->GetEntries();
 
@@ -37,76 +40,74 @@ Int_t split_channels(Int_t firstFile, Int_t lastFile)
 
     // =================================================================================
 
-    UInt_t Ks = 0, Kl = 0, Ksregen = 0, piplusks = 0, pipluskl = 0, piminusks = 0, piminuskl = 0,
-           muonplusks = 0, muonpluskl = 0, muonminusks = 0, muonminuskl = 0, electronks = 0, electronkl = 0,
-           positronks = 0, positronkl = 0, pi0ks = 0, pi0kl = 0, pi0phi = 0, piplusphi = 0, piminusphi = 0, otherphi = 0,
-           otherkl = 0, otherks = 0, gammaphi = 0;
-
     Bool_t signal_cond, regen_cond, omega_cond, three_cond, semi_cond, other_cond, pipi_cond;
 
     for (Int_t i = 0; i < nentries; i++)
     {
         chain->GetEntry(i);
 
-        interfcommon_.mcflag = 1;
+        UInt_t Ks = 0, Kl = 0, Ksregen = 0, piplusks = 0, pipluskl = 0, piminusks = 0, piminuskl = 0,
+           muonplusks = 0, muonpluskl = 0, muonminusks = 0, muonminuskl = 0, electronks = 0, electronkl = 0,
+           positronks = 0, positronkl = 0, pi0ks = 0, pi0kl = 0, pi0phi = 0, piplusphi = 0, piminusphi = 0, otherphi = 0,
+           otherkl = 0, otherks = 0, gammaphi = 0;
 
-        if (interfcommon_.mcflag == 1 /*&& baseKin.mctruth != 0 && baseKin.mctruth != 2*/)
+        if (mcflag == 1 && mctruth != 0 && mctruth != 2)
         {
             for (Int_t j = 0; j < interfcommon_.ntmc; j++)
             {
-                if (interfcommon_.mother[interfcommon_.vtxmc[j] - 1] == 50)
+                if (mother[vtxmc[j] - 1] == 50)
                 {
-                    if (interfcommon_.pidmc[j] == 10)
+                    if (pidmc[j] == 10)
                         Kl++;
-                    else if (interfcommon_.pidmc[j] == 16)
+                    else if (pidmc[j] == 16)
                         Ks++;
-                    else if (interfcommon_.pidmc[j] == 7)
+                    else if (pidmc[j] == 7)
                         pi0phi++;
-                    else if (interfcommon_.pidmc[j] == 8)
+                    else if (pidmc[j] == 8)
                         piplusphi++;
-                    else if (interfcommon_.pidmc[j] == 9)
+                    else if (pidmc[j] == 9)
                         piminusphi++;
-                    else if (interfcommon_.pidmc[j] == 1)
+                    else if (pidmc[j] == 1)
                         gammaphi++;
                     else
                         otherphi++;
                 }
-                else if (interfcommon_.mother[interfcommon_.vtxmc[j] - 1] == 10)
+                else if (mother[vtxmc[j] - 1] == 10)
                 {
-                    if (interfcommon_.pidmc[j] == 16)
+                    if (pidmc[j] == 16)
                         Ksregen++;
-                    else if (interfcommon_.pidmc[j] == 7)
+                    else if (pidmc[j] == 7)
                         pi0kl++;
-                    else if (interfcommon_.pidmc[j] == 8)
+                    else if (pidmc[j] == 8)
                         pipluskl++;
-                    else if (interfcommon_.pidmc[j] == 9)
+                    else if (pidmc[j] == 9)
                         piminuskl++;
-                    else if (interfcommon_.pidmc[j] == 5)
+                    else if (pidmc[j] == 5)
                         muonpluskl++;
-                    else if (interfcommon_.pidmc[j] == 6)
+                    else if (pidmc[j] == 6)
                         muonminuskl++;
-                    else if (interfcommon_.pidmc[j] == 2)
+                    else if (pidmc[j] == 2)
                         positronkl++;
-                    else if (interfcommon_.pidmc[j] == 3)
+                    else if (pidmc[j] == 3)
                         electronkl++;
                     else
                         otherkl++;
                 }
-                else if (interfcommon_.mother[interfcommon_.vtxmc[j] - 1] == 16)
+                else if (mother[vtxmc[j] - 1] == 16)
                 {
-                    if (interfcommon_.pidmc[j] == 7)
+                    if (pidmc[j] == 7)
                         pi0ks++;
-                    else if (interfcommon_.pidmc[j] == 8)
+                    else if (pidmc[j] == 8)
                         piplusks++;
-                    else if (interfcommon_.pidmc[j] == 9)
+                    else if (pidmc[j] == 9)
                         piminusks++;
-                    else if (interfcommon_.pidmc[j] == 5)
+                    else if (pidmc[j] == 5)
                         muonplusks++;
-                    else if (interfcommon_.pidmc[j] == 6)
+                    else if (pidmc[j] == 6)
                         muonminusks++;
-                    else if (interfcommon_.pidmc[j] == 2)
+                    else if (pidmc[j] == 2)
                         positronks++;
-                    else if (interfcommon_.pidmc[j] == 3)
+                    else if (pidmc[j] == 3)
                         electronks++;
                     else
                         otherks++;
@@ -149,6 +150,8 @@ Int_t split_channels(Int_t firstFile, Int_t lastFile)
                           (pi0kl == 2 && piplusks == 1 && muonminusks == 1 && pi0ks == 0) ||
                           (pi0kl == 2 && piminusks == 1 && muonplusks == 1 && pi0ks == 0)));
 
+            std::cout << signal_cond << " " << regen_cond << " " << omega_cond << " " << three_cond << " " << semi_cond << " " << pipi_cond << std::endl;
+
             if (signal_cond)
                 baseKin.mctruth_int = 1;
             else if (regen_cond)
@@ -164,7 +167,7 @@ Int_t split_channels(Int_t firstFile, Int_t lastFile)
             else
                 baseKin.mctruth_int = 7;
         }
-        else if (interfcommon_.mcflag == 0)
+        else if (mcflag == 0)
             baseKin.mctruth_int = 0;
 
         tree->Fill();
