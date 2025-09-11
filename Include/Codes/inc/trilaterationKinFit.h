4d2d4b39b94bc57124d489f7b45946f1c0cf74f6
Szymon Gamrat <szymon.gamrat@doctoral.uj.edu.pl>
2025-09-11 14:38:29 +0200

    Klasa działa

--- Include/Codes/inc/trilaterationKinFit.h
+++ Include/Codes/inc/trilaterationKinFit.h
@@ -11,108 +11,108 @@
 
 namespace KLOE
 {
-    class TrilaterationReconstructionKinFit : public KinFitter, protected ChargedVtxRec<> 
-    {
-    private:
-        TVectorD
-            _X,
-            _C,
-            _X_final,
-            _L,
-            _CORR,
-            _X_init,
-            _X_min,
-            _C_min,
-            _L_min,
-            _C_aux,
-            _L_aux,
-            _X_init_min,
-            _X_init_aux;
+	class TrilaterationReconstructionKinFit : public KinFitter, protected ChargedVtxRec<>
+	{
+	private:
+		TVectorD
+				_X,
+				_C,
+				_X_final,
+				_L,
+				_CORR,
+				_X_init,
+				_X_min,
+				_C_min,
+				_L_min,
+				_C_aux,
+				_L_aux,
+				_X_init_min,
+				_X_init_aux;
 
-        TMatrixD
-            _V,
-            _D,
-            _D_T,
-            _V_final,
-            _V_aux,
-            _V_min,
-            _Aux,
-            _V_invert,
-            _V_init;
+		TMatrixD
+				_V,
+				_D,
+				_D_T,
+				_V_final,
+				_V_aux,
+				_V_min,
+				_Aux,
+				_V_invert,
+				_V_init;
 
-        Double_t
-            _min_value_def,
-            _FUNVALMIN,
-            _CHISQRMIN,
-            _Chi2TriKinFit;
+		Double_t
+				_min_value_def,
+				_FUNVALMIN,
+				_CHISQRMIN,
+				_Chi2TriKinFit;
 
-        Int_t
-            _bunchnum,
-            _jmin,
-            _jmax;
+		Int_t
+				_bunchnum,
+				_jmin,
+				_jmax;
 
-        Bool_t
-            _isConverged;
+		Bool_t
+				_isConverged;
 
-        std::unique_ptr<Int_t[]>
-            _ind_gam,
-            _selected;
+		std::unique_ptr<Int_t[]>
+				_ind_gam;
 
-        std::vector<Float_t>
-            _cluster[5],
-            _bhabha_mom,
-            _bhabha_mom_err,
-            _bhabha_vtx,
-            _Param,
-            _Errors,
-            _iptri_kinfit,
-            _fourKnetri_kinfit,
-            _neu_vtx_min,
-            _gamma_mom_final[4];
+		std::vector<Float_t>
+				_cluster[5],
+				_bhabha_mom,
+				_bhabha_mom_err,
+				_bhabha_vtx,
+				_Param,
+				_Errors,
+				_iptri_kinfit,
+				_fourKnetri_kinfit,
+				_neu_vtx_min,
+				_gamma_mom_final[4];
 
-        std::vector<Int_t>
-            _NeuClusters,
-            _g4takentri_kinfit;
+		std::vector<Int_t>
+				_NeuClusters,
+				_g4takentri_kinfit,
+				_selected;
 
-        ConfigManager
-            &_config = ConfigManager::getInstance();
+		ConfigManager
+				&_config = ConfigManager::getInstance();
 
-        TrilaterationCode
-                    _recMode;
+		TrilaterationCode
+				_recMode;
 
-        Reconstructor
-            _R;
-        Solution
-            _S;
+		Reconstructor
+				_R;
+		Solution
+				_S;
 
-    public:
-        TrilaterationReconstructionKinFit(Int_t N_free, Int_t N_const, Int_t M, Int_t loopcount, Double_t chiSqrStep, Int_t jmin, Int_t jmax, ErrorHandling::ErrorLogs &logger);
-        ~TrilaterationReconstructionKinFit();
+	public:
+		TrilaterationReconstructionKinFit(Int_t N_free, Int_t N_const, Int_t M, Int_t loopcount, Double_t chiSqrStep, Int_t jmin, Int_t jmax, ErrorHandling::ErrorLogs &logger);
+		~TrilaterationReconstructionKinFit();
 
-        void SetParameters(const std::vector<Float_t> clu[5], const std::vector<Int_t> NeuClusters, const std::vector<Float_t> bhabha_mom, const std::vector<Float_t> bhabha_mom_err, const std::vector<Float_t> bhabha_vtx) 
-        { 
-            for (Int_t i = 0; i < 5; i++) 
-                _cluster[i].assign(clu[i].begin(), clu[i].end());
+		void SetParameters(const std::vector<Float_t> clu[5], const std::vector<Int_t> NeuClusters, const std::vector<Float_t> bhabha_mom, const std::vector<Float_t> bhabha_mom_err, const std::vector<Float_t> bhabha_vtx)
+		{
+			for (Int_t i = 0; i < 5; i++)
+				_cluster[i].assign(clu[i].begin(), clu[i].end());
 
-            _NeuClusters = NeuClusters;
-            _bhabha_mom = bhabha_mom;
-            _bhabha_mom_err = bhabha_mom_err;
-            _bhabha_vtx = bhabha_vtx;
-        }
+			_NeuClusters = NeuClusters;
+			_bhabha_mom = bhabha_mom;
+			_bhabha_mom_err = bhabha_mom_err;
+			_bhabha_vtx = bhabha_vtx;
+		}
 
-        void SetResults(Int_t &bunchnum, std::vector<Float_t> &iptri_kinfit, std::vector<Int_t> &g4takentri_kinfit, std::vector<Float_t> gamma_mom_final[4], std::vector<Float_t> &fourKnetri_kinfit, std::vector<Float_t> &neu_vtx_min, Float_t &Chi2TriKinFit)
-        { 
-            _bunchnum = bunchnum;
-            _iptri_kinfit = iptri_kinfit;
-            _g4takentri_kinfit = g4takentri_kinfit;
-            for (Int_t i = 0; i < 4; i++) 
-                _gamma_mom_final[i] = gamma_mom_final[i];
-            _fourKnetri_kinfit = fourKnetri_kinfit;
-            _neu_vtx_min = neu_vtx_min;
-            _Chi2TriKinFit = Chi2TriKinFit; 
-        }
+		void GetResults(Int_t &bunchnum, std::vector<Float_t> &iptri_kinfit, std::vector<Int_t> &g4takentri_kinfit, std::vector<Float_t> gamma_mom_final[4], std::vector<Float_t> &fourKnetri_kinfit, std::vector<Float_t> &neu_vtx_min, Float_t &Chi2TriKinFit)
+		{
+			bunchnum = _bunchnum;
+			iptri_kinfit = _iptri_kinfit;
+			g4takentri_kinfit = _g4takentri_kinfit;
+			for (Int_t i = 0; i < 4; i++)
+				gamma_mom_final[i] = _gamma_mom_final[i];
+			fourKnetri_kinfit = _fourKnetri_kinfit;
+			neu_vtx_min = _neu_vtx_min;
+			Chi2TriKinFit = _Chi2TriKinFit;
+		}
 
-        ErrorHandling::ErrorCodes Reconstruct();
-    };
+		ErrorHandling::ErrorCodes Reconstruct();
+	};
 
 } // namespace KLOE
