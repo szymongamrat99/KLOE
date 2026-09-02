58abb8ccf4ead3c13486e064eeecea7a5fedef2c
Szymon Gamrat <szymon.gamrat@doctoral.uj.edu.pl>
2026-09-02 22:07:57 +0200

    change: change for tier1

--- execute_analysis.sh
+++ execute_analysis.sh
@@ -11,10 +11,6 @@ then
   set +a
 fi
 
-if [ "$HOST" == "ui-tier1.cr.cnaf.infn.it" ]; then
-  ../copy_libs.sh
-fi
-
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j$nproc
@@ -22,3 +18,7 @@ make -j$nproc
 echo "Using ANALYSIS_CONFIG_FILE: $ANALYSIS_CONFIG_FILE"
 export ANALYSIS_CONFIG_FILE=${ANALYSIS_CONFIG_FILE}
 ./bin/KLSPM00 $file_list
+
+if [ "$HOST" == "ui-tier1.cr.cnaf.infn.it" ]; then
+  ../copy_libs.sh
+fi
